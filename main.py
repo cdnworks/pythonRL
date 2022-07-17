73fddd7345e12bb56a9677973ec2d42a2db9d706
cdnworks <chrisdnielson@gmail.com>
2022-07-17 00:55:36 -0700

    Map generation
    added basic map generation

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 
 def main() -> None:
@@ -14,6 +14,10 @@ def main() -> None:
 
     map_width = 80
     map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
 
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -29,7 +33,14 @@ def main() -> None:
     entities = {test_npc, player}
 
     # Initialize game map
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(
+        max_rooms=max_rooms,
+        room_min_size=room_min_size,
+        room_max_size=room_max_size,
+        map_width=map_width,
+        map_height=map_height,
+        player=player
+    )
 
     # Initialize the game engine
     engine = Engine(entities=entities, event_handler=event_handler, 

ee733ce935daf0f65da7a39cc7558ee673179ee4
cdnworks <chrisdnielson@gmail.com>
2022-07-26 16:57:27 -0700

    Main Menu
    added a main menu, and an initializer class that sets up the engine for a new game.

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3
-import copy
 import traceback
+from setuptools import setup
 
 import tcod
 
 import color
-from engine import Engine
-import entity_factories
 import exceptions
 import input_handlers
-from procgen import generate_dungeon
+import setup_game
 
 
 def main() -> None:
@@ -17,50 +15,11 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    map_width = 80
-    map_height = 43
-
-    # Procgen params
-    room_max_size = 10
-    room_min_size = 6
-    max_rooms = 30
-
-    # Mobgen params
-    max_monsters_per_room = 2
-
-    # Itemgen params
-    max_items_per_room = 2
-
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    # Initialize some basic entities
-    player = copy.deepcopy(entity_factories.player)
-    
-    # Initialize the game engine
-    engine = Engine(player=player)
-
-    # Initialize game map
-    engine.game_map = generate_dungeon(
-        max_rooms=max_rooms,
-        room_min_size=room_min_size,
-        room_max_size=room_max_size,
-        map_width=map_width,
-        map_height=map_height,
-        max_monsters_per_room=max_monsters_per_room,
-        max_items_per_room=max_items_per_room,
-        engine=engine
-    )
-
-    engine.update_fov()
-
-    engine.message_log.add_message(
-        "Hello and welcome, adventurer, to yet another dungeon!", 
-        color.welcome_text
-    )
-
-    handler: input_handlers.BaseEventHandler = input_handlers.MainGameEventHandler(engine)
+    handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
 
     with tcod.context.new_terminal(
         screen_width,

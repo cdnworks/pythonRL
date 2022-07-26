9565930e9b1a56e6560bcac9c5407b0f1b5f80c1
cdnworks <chrisdnielson@gmail.com>
2022-07-26 16:19:45 -0700

    Refactoring
    created a new ActionOrHandler class so we no longer have to set self.engine.event_handler any time we perform an event in runtime. Instead we can just change the return type to ActionOrHandler.

--- exceptions.py
+++ exceptions.py
@@ -5,3 +5,10 @@ class Impossible(Exception):
     The reason is given as the exception message.
     i.e. raise Impossible("An exception message") 
     '''
+
+
+class QuitWithoutSaving(SystemExit):
+    '''
+    This exception should be raised to exit the game without saving.
+    i.e. raise QuitWithoutSaving when the player dies.
+    '''
